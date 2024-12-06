127f9a0f5b05156b43bf8a1dec04b3f8359062ba
SanderSander <themastersleader@hotmail.com>
2024-12-06 20:13:55 +0100

    Last test

---
+++ tests/Unit/Repository/RepositoryFactoryTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the composer-link plugin.
+ *
+ * Created by: Sander Visser <themastersleader@hotmail.com>.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ *
+ * @link https://github.com/SanderSander/composer-link
+ */
+
+namespace Tests\Unit\Repository;
+
+use ComposerLink\Repository\RepositoryFactory;
+use Tests\Unit\TestCase;
+
+class RepositoryFactoryTest extends TestCase
+{
+    public function test_creates_repository(): void
+    {
+        $factory = new RepositoryFactory();
+        $factory->create($this->tmpAbsoluteDir . 'linked-packages.json');
+
+        static::expectNotToPerformAssertions();
+    }
+}
