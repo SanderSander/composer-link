f5e02f1714cd290b54b0e53c32e3122078d5c97e
Sander Visser <themastersleader@hotmail.com>
2026-01-19 14:05:08 +0100

    Fix issue where unlink leaves the packages in an updated state (#90)

--- src/LinkManager.php
+++ src/LinkManager.php
@@ -184,7 +184,7 @@ class LinkManager
         $installer = $this->installerFactory->create();
 
         /* @phpstan-ignore method.deprecated */
-        $installer->setUpdate(true)
+        $installer->setUpdate(count($this->requires) > 0)
             ->setInstall(true)
             ->setWriteLock(false)
             ->setRunScripts(false)
